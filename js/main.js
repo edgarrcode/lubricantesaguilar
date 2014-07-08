be6852546e6d32f345d753baea7fcf063b7a5bd2
Edgar Rodriguez <1983eerr@gmail.com>
2014-07-07 20:27:26 -0600

    redesign

--- js/main.js
+++ js/main.js
@@ -46,7 +46,61 @@ setInterval(function() {
     losLogos.toggleClass('setB');
 },4000);
 
-// VideoHook
-$().ready(function() {
-        $('#elContenido').tubular({videoId: 'Jo5fPXLlko4'});
-});
+// Youtbe
+//Remove div if mobile
+if ($(window).width() < 768) {
+  $('#theIfraeWrapper, #theIframeShield').remove();
+}
+
+      // 2. This code loads the IFrame Player API code asynchronously.
+      var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+            height: '100%',
+            width: '100%',
+            videoId: 'Jo5fPXLlko4',
+                playerVars: {
+                    controls: 0,
+                    showinfo: 0,
+                    modestbranding: 1,
+                    wmode: 'transparent',
+                    loop: 1,
+                    playlist: 'Jo5fPXLlko4',
+                },
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange,
+            }
+        });
+      }
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.playVideo();
+        event.target.setVolume(0);
+
+      }
+
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+
+      }
+
